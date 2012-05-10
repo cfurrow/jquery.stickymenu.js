0bcd5d9ff7ef2afec871977ebcae62b242225d32
Carl Furrow <me@carlfurrow.com>
2012-05-09 22:34:46 -0400

    Added demo folder. updated lib to wrap in another element

--- jquery.stickymenu.js
+++ jquery.stickymenu.js
@@ -1,23 +1,34 @@
-!function($){
+!(function($){
   if(typeof $ === "undefined" || $ === null){
     return;
   }
 
   $.fn.stickyMenu = function(options){
-		if(this.length == 0){
-			return this;
-		}
+    if(!options){ options = {}; }
+    if(this.length === 0){
+      return this;
+    }
+    var DEFAULTS = {
+      additionalOffset:0
+    };
+    $.extend(options,DEFAULTS);
     var additionalOffset = options.additionalOffset;
     var $window = $(window);
-    var $menu = this;
-    var offset = $menu.offset();  
+    var originalWidth = this.width();
+    this.wrap("<div class='sticky-wrapper'></div>");
+    var $original = this;
+    var $menu = $(".sticky-wrapper");
+    var offset = $menu.offset();
+
     $window.scroll(function(){
       if($window.scrollTop() > offset.top + $menu.height() + additionalOffset && !$menu.hasClass('fixed')){
         $menu.addClass('fixed');
+        $original.width(originalWidth); 
       }
       else if($window.scrollTop() + additionalOffset < offset.top && $menu.hasClass('fixed')){
         $menu.removeClass('fixed');
+        $original.width("auto"); 
       }
     });
   };
-}(window.jQuery)
+})(window.jQuery);
