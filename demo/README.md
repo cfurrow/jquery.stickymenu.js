e4221b86e9dd7d7096d67e17b42c9530932be423
Carl Furrow <me@carlfurrow.com>
2012-07-14 23:07:16 -0400

    Adding demo folder

---
+++ demo/README.md
@@ -0,0 +1,13 @@
+# jquery.stickymenu.js
+
+## What is it?
+This jquery plugin allows you to stick a menu item, or any other block element to the top of your page, even when users scrolls down! It "sticks" to the top of the page.
+
+[You can see it in action in the demo](demo/index.html)
+
+## What are the prerequirements?
+* jquery (tested with 1.7.2)
+
+## Show me!
+[Ok](demo/index.html)
+
---
+++ demo/jquery.stickymenu.js
@@ -0,0 +1,34 @@
+(function($){
+  if(typeof $ === "undefined" || $ === null){
+    return;
+  }
+
+  $.fn.stickyMenu = function(options){
+    if(!options){ options = {}; }
+    if(this.length === 0){
+      return this;
+    }
+    var DEFAULTS = {
+      additionalOffset:0
+    };
+    $.extend(options,DEFAULTS);
+    var additionalOffset = options.additionalOffset;
+    var $window = $(window);
+    var originalWidth = this.width();
+    this.wrap("<div class='sticky-wrapper'></div>");
+    var $original = this;
+    var $menu = $(".sticky-wrapper");
+    var offset = $menu.offset();
+
+    $window.scroll(function(){
+      if($window.scrollTop() > offset.top + $menu.height() + additionalOffset && !$menu.hasClass('fixed')){
+        $menu.addClass('fixed');
+        $original.width(originalWidth);
+      }
+      else if($window.scrollTop() + additionalOffset < offset.top && $menu.hasClass('fixed')){
+        $menu.removeClass('fixed');
+        $original.width("auto");
+      }
+    });
+  };
+})(window.jQuery);
